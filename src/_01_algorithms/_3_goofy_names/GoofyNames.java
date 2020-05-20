037bf8092187171f22560fbcce3a2500e911e681
root <root@9cf270513f16>
2020-05-20 21:57:41 +0000

    hi

--- src/_01_algorithms/_3_goofy_names/GoofyNames.java
+++ src/_01_algorithms/_3_goofy_names/GoofyNames.java
@@ -11,26 +11,35 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-JOptionPane.showInputDialog(null, "What is your name?");
+String N = JOptionPane.showInputDialog(null, "What is your name?");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-
+N=N.toUpperCase();
+JOptionPane.showMessageDialog(null, N);
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
 
+for (int i = 0; i < N.length(); i++) {
+	
+
 			// 4. Create a String variable to store the next character of the name
-			//    using .substring(start, end)
-			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
-			// 5. Use MODULO to set this variable to upper case for EVEN characters
-			//    and lower case for ODD characters.
-
-			
-			// 6. ADD this String  (containing 1 char) to the goofyName String
-
+		   //    using .substring(start, end)
+		  //    HINT: replace 'start' and 'end' to get String with character at i 
+		String G=N.substring(i,i+1);
 		
+		// 5. Use MODULO to set this variable to upper case for EVEN characters
+       //    and lower case for ODD characters.
+		if(i%2==0) {
+		G=G.toUpperCase();
+		}else {
+			
+		G=G.toLowerCase();
+		}
+			// 6. ADD this String  (containing 1 char) to the goofyName String
+goofyName=goofyName+G;
+	}	
 		// 7. Use pop-up to show user their Goofy name
-
+JOptionPane.showMessageDialog(null, goofyName);
 	}
 }
 
--- src/_02_nested_loops/_1_snowflake/Snowflake.java
+++ src/_02_nested_loops/_1_snowflake/Snowflake.java
@@ -2,54 +2,69 @@ package _02_nested_loops._1_snowflake;
 
 import java.awt.Color;
 
+import org.jointheleague.graphical.robot.Robot;
+
 
 public class Snowflake {
 	
 	public static void main(String[] args) {
 
 		// #1. Make a new Robot
-
+Robot ice= new Robot("batman");
 
 		// #2. Set your robot’s position to x=300 and y=300
-
-
+ ice.setX(300);
+ ice.setY(300);
 		// #3. Put the robot's pen down
-
+ice.penDown();
 
 		// #4. Set the robot’s speed to 5
-
+ice.setSpeed(5);
 
 		// #5. use a for loop below to do everything ( #6, #7, #8, #9, #12, #13, #14 ) 6 times (use i as the counter)
+for (int i = 0; i < 6; i++) {
+	
 
 			
 			// #6. Set the pen color to black
-
+ice.setPenColor(Color.black);
 			
 			// #7. move the robot by 50
-
+ice.move(50);
 			
 			// #8. turn the robot to the right by 60
+			ice.turn(60);
 			
 			
-			// #12. set the pen color to blue for even numbers of i
-
-			
+		// #12. set the pen color to blue for even numbers of i
+			if(i%2==0) {
+			ice.setPenColor(Color.cyan);
+			}else {
+				ice.setPenColor(Color.red);
+				
+			}
 			// #13. set the pen color to red for all odd numbers of i
 			
 			
 			// #9. Use a for loop to do everything (#10, #11) 6 times ( use j as a counter )
-			
-			
-					// #10. turn the robot to the left by 60
 		
-					
+	
+	
+	for (int j = 0; j < 6; j++) {
+		
+	
+
+					// #10. turn the robot to the left by 60
+		ice.turn(-60);
+	
 					// #11. move the robot by 50 (play the start button)
-					
+ice.move(50);
+	}
 				
-			
-			// #14. set the pen color to black
+//#14. set the pen color to black
+		ice.setPenColor(Color.black);	
 				
 		
 	}
-
+	}
 }
