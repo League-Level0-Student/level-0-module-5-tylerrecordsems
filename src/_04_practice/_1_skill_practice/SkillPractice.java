3bdecb0d3d9980dd6d39a7ea5181c5d05af4c392
root <root@c5f082ee5800>
2020-07-01 21:55:58 +0000

    hi

--- src/_04_practice/_1_skill_practice/SkillPractice.java
+++ src/_04_practice/_1_skill_practice/SkillPractice.java
@@ -1,14 +1,16 @@
 package _04_practice._1_skill_practice;
 
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class SkillPractice {
 public static void main(String[] args) {
 	SkillPractice skills = new SkillPractice();
-    skills.skill1();
-   // skills.skill2();
-   // skills.skill3();
-   // skills.skill4();
+   // skills.skill1();
+    //skills.skill2();
+    //skills.skill3();
+    skills.skill4();
     //skills.skill5();
 }
 
@@ -25,13 +27,36 @@ int num=Integer.parseInt(p);
 if(num <= 36) {
 	 JOptionPane.showMessageDialog(null,"Eat your Wheaties");
 }
+}
 	// If they are shorter than 36 inches, tell them to eat their Wheaties
 
+ 
+// Write a loop to print every third number between 1 and 30 to the console 
+void skill2() {
+for (int i = 1; i < 31; i+=3) {
+	System.out.println(i);
+}
+}
+void skill3() {
+	Random num=new Random();
+	int Gummy= num.nextInt(21);
+System.out.println(Gummy);
+int Slime=num.nextInt(10);	
+System.out.println(Slime);
+JOptionPane.showMessageDialog(null, "The difference between the two numbers are"+(Gummy - Slime ));
+}
+void skill4() {
+String f=	JOptionPane.showInputDialog("Which city do you live in? (No caps!)");
+if(f=="san diego") {
+	JOptionPane.showInputDialog("You live in America's Finest City ");
+}else {
+	JOptionPane.showInputDialog("Move to San Diego");
+}
+}
 
-
-
-	}
-	
-	
 
 }
+
+	
+
+
